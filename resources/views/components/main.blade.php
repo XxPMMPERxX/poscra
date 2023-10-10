5b8db97513bac8a0e088d837848128a25adc7c62
gollilla <soradore2@gmail.com>
2023-10-11 01:31:15 +0900

    add: ナビバー部分をコンポーネント化　メニュー部分をドロップダウンに　カードをホバー時に透過率を変えるように

---
+++ resources/views/components/main.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>ポスクラ | トップページ</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body class="antialiased">
+        <div class="relative sm:flex sm:flex-col min-h-screen bg-center bg-mywhite">
+            <div class="flex w-full justify-between items-center">
+                <div class="font-cpfont text-4xl ml-9 text-myaccent">
+                    <a href="{{ url('/') }}">ポスクラ</a>
+                </div>
+                <div class="sm:top-0 sm:right-0 pr-7 mr-7 h-11 leading-10 my-10 text-mydark font-yusei">
+                    @auth
+                        <details class="dropdown dropdown-end">
+                            <summary class="flex items-center cursor-pointer">
+                                <img class="mx-2" src="{{ Vite::asset('resources/images/icon_steve.png') }}" />
+                                <div>{{ auth()->user()->name }}</div>
+                            </summary>
+                            <ul class="p-2 shadow menu dropdown-content z-[1] bg-mywhite rounded-box w-52">
+                                <li><a href="{{ url('/dashboard') }}">ダッシュボード</a></li>
+                                <li><a>設定</a></li>
+                                <li class="text-[#e66c51]"><a>ログアウト</a></li>
+                            </ul>
+                        </details>
+                    @else
+                        <a href="{{ route('google_auth') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500 flex items-center">
+                            <img class="mx-2" src="{{ Vite::asset('resources/images/accessibility_glyph_color.png') }}" />
+                            ログイン / 登録
+                        </a>
+                    @endauth
+                </div>
+            </div>
+            {{ $slot }}
+        </div>
+    </body>
+</html>
