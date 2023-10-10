c1455e4f69059e3c457b729b8b43b35bf3dd0978
gollilla <soradore2@gmail.com>
2023-10-10 16:40:26 +0900

    add: カルーセルアイテムのカーソルをポインターに変更

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>ポスクラ | トップページ</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
@@ -35,7 +35,7 @@
                 <div class="font-yusei lg:text-[2vw] text-mydark text-center mb-5 text-[20px]" style="text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);">トレンド</div>
                 <div class="carousel ml-[24px] 2xl:w-[1390px] 2xl:mx-auto 2xl:flex">
                     <!-- Open the modal using ID.showModal() method -->
-                    <div class="carousel-item sm:mx-[12px] mx-[5px] relative">
+                    <div class="carousel-item sm:mx-[12px] mx-[5px] relative cursor-pointer">
                         <div class="absolute text-mydark font-yusei sm:text-[14px] bg-myaccent sm:w-[34px] sm:h-[26px] text-center sm:leading-[26px] rounded-tl-[20px] text-[10px] w-[25px] h-[18px] leading-[18px]">1位</div>
                         <img onclick="openModal('my_modal_1', 'test123123', 'test')" class="sm:w-[330px] sm:h-[185px] rounded-[20px] w-[178px] h-[100px]" src="{{ Vite::asset('resources/images/default_thumbnail.png') }}" />
                     </div>
@@ -48,7 +48,7 @@
                             <button>close</button>
                         </form>
                     </dialog>
-                    <div class="carousel-item sm:mx-[12px] mx-[5px] relative">
+                    <div class="carousel-item sm:mx-[12px] mx-[5px] relative cursor-pointer">
                         <div class="absolute text-mydark font-yusei sm:text-[14px] bg-myaccent sm:w-[34px] sm:h-[26px] text-center sm:leading-[26px] rounded-tl-[20px] text-[10px] w-[25px] h-[18px] leading-[18px]">2位</div>
                         <img onclick="openModal('my_modal_2', 'test1231233', 'test2')" class="sm:w-[330px] sm:h-[185px] rounded-[20px] w-[178px] h-[100px]" src="{{ Vite::asset('resources/images/default_thumbnail.png') }}" />
                     </div>
@@ -61,7 +61,7 @@
                             <button>close</button>
                         </form>
                     </dialog>
-                    <div class="carousel-item sm:mx-[12px] mx-[5px] relative">
+                    <div class="carousel-item sm:mx-[12px] mx-[5px] relative cursor-pointer">
                         <div class="absolute text-mydark font-yusei sm:text-[14px] bg-myaccent sm:w-[34px] sm:h-[26px] text-center sm:leading-[26px] rounded-tl-[20px] text-[10px] w-[25px] h-[18px] leading-[18px]">3位</div>
                         <img onclick="openModal('my_modal_3', 'test1231233', 'test3')" class="sm:w-[330px] sm:h-[185px] rounded-[20px] w-[178px] h-[100px]" src="{{ Vite::asset('resources/images/default_thumbnail.png') }}" />
                     </div>
@@ -74,7 +74,7 @@
                             <button>close</button>
                         </form>
                     </dialog>
-                    <div class="carousel-item sm:mx-[12px] mx-[5px] relative">
+                    <div class="carousel-item sm:mx-[12px] mx-[5px] relative cursor-pointer">
                         <div class="absolute text-mydark font-yusei sm:text-[14px] bg-myaccent sm:w-[34px] sm:h-[26px] text-center sm:leading-[26px] rounded-tl-[20px] text-[10px] w-[25px] h-[18px] leading-[18px]">4位</div>
                         <img onclick="openModal('my_modal_4', 'test12312334', 'test4')" class="sm:w-[330px] sm:h-[185px] rounded-[20px] w-[178px] h-[100px]" src="{{ Vite::asset('resources/images/default_thumbnail.png') }}" />
                     </div>
@@ -92,7 +92,7 @@
             <div id="new" class="mt-5">
                 <div class="font-yusei lg:text-[2vw] text-mydark text-center mb-5 text-[20px]" style="text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);">新着</div>
                 <div class="carousel ml-[24px] 2xl:w-[1390px] 2xl:mx-auto 2xl:flex">
-                    <div class="carousel-item sm:mx-[12px] mx-[5px]">
+                    <div class="carousel-item sm:mx-[12px] mx-[5px] cursor-pointer">
                         <img onclick="openModal('my_modal_10', 'test123123', 'test')" class="sm:w-[330px] sm:h-[185px] rounded-[20px] w-[178px] h-[100px]" src="{{ Vite::asset('resources/images/default_thumbnail.png') }}" />
                     </div>
                     <dialog id="my_modal_10" class="modal">
@@ -104,7 +104,7 @@
                             <button>close</button>
                         </form>
                     </dialog>
-                    <div class="carousel-item sm:mx-[12px] mx-[5px]">
+                    <div class="carousel-item sm:mx-[12px] mx-[5px] cursor-pointer">
                         <img onclick="openModal('my_modal_2', 'test1231233', 'test2')" class="sm:w-[330px] sm:h-[185px] rounded-[20px] w-[178px] h-[100px]" src="{{ Vite::asset('resources/images/default_thumbnail.png') }}" />
                     </div>
                     <dialog id="my_modal_2" class="modal">
@@ -116,7 +116,7 @@
                             <button>close</button>
                         </form>
                     </dialog>
-                    <div class="carousel-item sm:mx-[12px] mx-[5px]">
+                    <div class="carousel-item sm:mx-[12px] mx-[5px] cursor-pointer">
                         <img onclick="openModal('my_modal_3', 'test1231233', 'test3')" class="sm:w-[330px] sm:h-[185px] rounded-[20px] w-[178px] h-[100px]" src="{{ Vite::asset('resources/images/default_thumbnail.png') }}" />
                     </div>
                     <dialog id="my_modal_3" class="modal">
@@ -128,7 +128,7 @@
                             <button>close</button>
                         </form>
                     </dialog>
-                    <div class="carousel-item sm:mx-[12px] mx-[5px]">
+                    <div class="carousel-item sm:mx-[12px] mx-[5px] cursor-pointer">
                         <img onclick="openModal('my_modal_4', 'test12312334', 'test4')" class="sm:w-[330px] sm:h-[185px] rounded-[20px] w-[178px] h-[100px]" src="{{ Vite::asset('resources/images/default_thumbnail.png') }}" />
                     </div>
                     <dialog id="my_modal_4" class="modal">
