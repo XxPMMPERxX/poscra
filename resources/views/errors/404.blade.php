132fc0ff487f2794dc600270f09ef0b21c415e8b
gollilla <soradore2@gmail.com>
2023-11-05 19:52:55 +0900

    fix: 404ページ修正

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,4 +1,5 @@
 <x-layout.main>
+    <x-slot:title>見つかりませんでした</x-slot:title>
     <div class="w-full h-full">
         <img class="w-full h-full" src="{{ Vite::asset('resources/images/404bg.png') }}" alt="">
     </div>
