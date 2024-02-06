784d0710ff5d5dd9c61d7d4439abb09a0805f7e4
soradore <soradore2@gmail.com>
2024-02-06 10:28:50 +0000

    fix: 運用環境用設定追加

--- app/Models/AdminUser.php
+++ app/Models/AdminUser.php
@@ -2,12 +2,14 @@
 
 namespace App\Models;
 
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class AdminUser extends Authenticatable
+class AdminUser extends Authenticatable implements FilamentUser
 {
     use HasFactory, Notifiable;
 
@@ -26,4 +28,9 @@ class AdminUser extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function canAccessPanel(Panel $panel): bool 
+    {
+        return str_ends_with($this->email, '@poscra.com');
+    }
 }
